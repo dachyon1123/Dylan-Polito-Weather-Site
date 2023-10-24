2e5354a97d928f2493ec3a6908330b2b5182c5b3
Dylan <dpolito1123@gmail.com>
2023-10-23 21:06:21 -0500

    added js for history and api

--- develop/script.js
+++ develop/script.js
@@ -1,11 +1,46 @@
+function renderWeather(weather) {
+  console.log(weather)
+  let cardOne = $('.card-1');
+  let cardOneText = $('.card-1-text')
+  cardOneText.text(weather.main.temp)
+  
+}
 
 
-function fetchWeather() {
-  let url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ded887f09c87ed8a14484e02ddb5d99e';
+
+
+
+
+function fetchWeather(city) {
+  let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ded887f09c87ed8a14484e02ddb5d99e";
 
   fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => renderWeather(data))
 }
 
-fetchWeather();
+
+
+
+
+$('#search-button').on('click', (event) => {
+  event.preventDefault();
+  let cityLocation = $('#default-search').val();
+  
+
+  fetchWeather(cityLocation);
+
+  let cityHistory = $('.search-history');
+
+  let cityHistoryText = $('<h3>')
+  cityHistoryText.text(cityLocation)
+
+  let cityHistoryCard = $('<div>')
+  cityHistoryCard.addClass('cityCard flex justify-center items-center bg-slate-200/[0.3] rounded-lg w-3/4 h-10')
+  cityHistory.append(cityHistoryCard)
+  cityHistoryCard.append(cityHistoryText)
+
+  
+  
+})
+
