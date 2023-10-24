da694334ed6a032721b698178fb27769a9ff19d6
Dylan <dpolito1123@gmail.com>
2023-10-24 17:30:15 -0500

    worked on getting lat/lon for the city that is inputed.

--- develop/script.js
+++ develop/script.js
@@ -1,45 +1,35 @@
-function renderWeather(weather) {
-  console.log(weather)
-  let cardOne = $('.card-1');
-  let cardOneText = $('.card-1-text')
-  cardOneText.text(weather.main.temp)
+function fetchWeather(geocode) {
+  let lat = geocode[0].lat
+  let lon = geocode[0].lon
+
+
+  let url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ded887f09c87ed8a14484e02ddb5d99e";
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+function handleFiveDayWeather() {
   
 }
 
-function renderCityGeo (geocode) {
-  console.log(geocode)
-  console.log(geocode[0].lat)
-  console.log(geocode[0].lon)
-}
+
+
 
 function geocodeCity(city) {
   let url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=ded887f09c87ed8a14484e02ddb5d99e"
 
   fetch(url)
   .then(response => response.json())
-  .then(data => renderCityGeo(data))
-}
-
-
-function fetchWeather(city) {
-  let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ded887f09c87ed8a14484e02ddb5d99e";
-
-  fetch(url)
-  .then(response => response.json())
-  .then(data => renderWeather(data))
-}
-
-
-
-
-
-$('#search-button').on('click', (event) => {
-  event.preventDefault();
-  let cityLocation = $('#default-search').val();
+  .then(data => {
+    fetchWeather(data)
+  })
   
-  geocodeCity(cityLocation)
-  fetchWeather(cityLocation);
+}
 
+
+function renderCityHistory(cityLocation) {
   let cityHistory = $('.search-history');
 
   let cityHistoryText = $('<h3>')
@@ -49,6 +39,17 @@ $('#search-button').on('click', (event) => {
   cityHistoryCard.addClass('cityCard flex justify-center items-center bg-slate-200/[0.3] rounded-lg w-3/4 h-10')
   cityHistory.append(cityHistoryCard)
   cityHistoryCard.append(cityHistoryText)
+}
+
+
+$('#search-button').on('click', (event) => {
+  event.preventDefault();
+  let cityLocation = $('#default-search').val();
+  
+  geocodeCity(cityLocation)
+
+  renderCityHistory(cityLocation)
+  
 
   
   
